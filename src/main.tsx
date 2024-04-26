4f45dac42ccb5008985fe87372a7212a1eb570d4
Haider <haideraliamjad4391@gmail.com>
2024-04-26 11:53:35 -0700

    add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "./index.css";
-
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,7 +9,15 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRef } from "react";
 export default function LoginPage() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  function login() {
+    console.log(emailRef.current?.value, passwordRef.current?.value);
+  }
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Card className="mx-auto max-w-sm">
@@ -28,6 +36,7 @@ export default function LoginPage() {
                 type="email"
                 placeholder="m@example.com"
                 required
+                ref={emailRef}
               />
             </div>
             <div className="grid gap-2">
@@ -40,9 +49,9 @@ export default function LoginPage() {
                   Forgot your password?
                 </Link>
               </div>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" ref={passwordRef} required />
             </div>
-            <Button type="submit" className="w-full">
+            <Button type="submit" onClick={login} className="w-full">
               Login
             </Button>
           </div>
